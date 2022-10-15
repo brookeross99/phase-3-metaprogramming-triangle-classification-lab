f5954628c2475eefaba392a0f85be2b6403731b0
Brooke Ross <brooke.ross2800@gmail.com>
2022-10-15 15:03:34 -0600

    commit tree ac60b8c9034c69f1c363c691aa0f1b78729d7cdf

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+  
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    validate_triangle
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def sides_greater_than_zero?
+    [a, b, c].all?(&:positive?)
+  end
+# The &:positive? Is a method that checks if all values are positive numbers. 
+
+  def valid_triangle_inequality?
+    a + b > c && a + c > b && b + c > a
+  end
+# Objective checking for inequality 
+
+  def validate_triangle
+    raise TriangleError unless sides_greater_than_zero? && valid_triangle_inequality?
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
